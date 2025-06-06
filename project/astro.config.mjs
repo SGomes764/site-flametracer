9cc1d6f249b5f8d6ca5eaaa874e1979d8780c3ec
Vasco Alexandre Maximiano Abrantes <52137@CTEAEBatalha.local>
2025-06-06 16:00:55 +0100

    teste

--- project/astro.config.mjs
+++ project/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://SGomes764.github.io',
+  site: 'https://sgomes764.github.io/site-flametracer', 
   base: '/site-flametracer',
   output: 'static'
 });
